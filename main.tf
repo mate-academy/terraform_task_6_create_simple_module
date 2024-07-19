6c6c1261046fac0054441581036715526fc1c7bf
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-19 09:13:13 +0300

    Solution

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "task6" {
+  source = "github.com/ILyakhova/terraform-azurerm-resource_group_storage"
+
+  resource_group_name   = "example-rg"
+  location              = "East US"
+  storage_account_name  = "examplestorage"
+}
---
+++ modules/resource_group_storage/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Resource group location"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Storage account name"
+  type        = string
+}
+
