c5dfb31a37d5d2af916a19f0b0e72ca47680160a
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-19 09:45:36 +0300

    fmt main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "resource_group_storage" {
   source = "github.com/ILyakhova/terraform-azurerm-resource_group_storage"
 
-  resource_group_name   = "example-rg"
-  location              = "East US"
-  storage_account_name  = "examplestorage"
+  resource_group_name  = "example-rg"
+  location             = "East US"
+  storage_account_name = "examplestorage"
 }
