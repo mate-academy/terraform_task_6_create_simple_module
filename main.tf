948b9c3d9326285c0ed53989bdbce94fe67f8601
Serveladus <serveladik@gmail.com>
2024-09-17 15:38:58 +0300

    Solution3

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "azurerm" {
 }
 
 module "resource_group_storage" {
-  source               = "./modules/resource_group_storage"
+  source = "Serveladik/resource_group_storage/azurerm"
+
   resource_group_name  = var.resource_group_name
   location             = var.location
   storage_account_name = var.storage_account_name
