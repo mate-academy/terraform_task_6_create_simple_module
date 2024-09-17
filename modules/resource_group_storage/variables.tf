031ea6660a7e1063037670d1019bbcd596d3f4a5
Serveladus <serveladik@gmail.com>
2024-09-17 15:45:49 +0300

    added module back

---
+++ modules/resource_group_storage/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "default-resource-group"
+}
+
+variable "location" {
+  type        = string
+  default     = "Poland Central"
+}
+
+variable "storage_account_name" {
+  type        = string
+  default     = "defaultstorageaccount"
+}
