42237f5030ce8bdb957ef38c679d1ed4d7c0e9bc
Darya-Savchenko <savchenkodarya277@gmail.com>
2024-11-26 15:40:16 +0200

    fix module name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
   features {}
 }
 
-module "task6_module" {
+module "resource_group_storage" {
   source               = "Darya-Savchenko/resource_group_storage/azurerm"
   version              = "1.0.0"
   resource_group_name  = "task6-resource-group"
