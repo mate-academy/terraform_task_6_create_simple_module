3ebb84dfe9b356aecd7ca8b6e3d0976a09bfad06
Darya-Savchenko <savchenkodarya277@gmail.com>
2024-11-26 15:34:21 +0200

    solution

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "task6_module" {
+  source               = "Darya-Savchenko/resource_group_storage/azurerm"
+  version              = "1.0.0"
+  resource_group_name  = "task6-resource-group"
+  storage_account_name = "task6storageaccount"
+  location             = "West US"
+}
+
